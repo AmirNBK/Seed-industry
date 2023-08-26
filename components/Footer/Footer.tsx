7de2024f40c8fa38066e45e06687796a6c29a43d
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-08-26 17:14:35 +0330

    some minor issues fixed

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import logo from '../../assets/Images/logo.png'
 import arrow from '../../assets/Icons/arrow-2.svg'
 import Link from 'next/link';
+import { Vazirmatn } from 'next/font/google';
+const vazir = Vazirmatn({ subsets: ['latin'] });
 
 const myFont = localFont({ src: '../../assets/Fonts/mj.ttf' })
 const Footer = () => {
@@ -17,22 +19,23 @@ const Footer = () => {
                 <div className='Footer__rightSide flex flex-col gap-6'>
                     <div className='text-end flex flex-col gap-4'>
                         <h3 className={`Footer__rightSide__title text-3xl ${myFont.className}`} style={{ color: '#EBDAB2' }}> فهرست صفحات </h3>
-                        <Link href={'/'} className='Footer__rightSide__subtitle sm:text-right text-center text-white text-xl'>
+                        <Link href={'/'}
+                            className={`${vazir.className} Footer__rightSide__subtitle sm:text-right text-center text-white text-xl`}>
                             صفحه اصلی
                         </Link>
-                        <Link href={'about-us'} className='Footer__rightSide__subtitle sm:text-right text-center text-white text-xl'>
+                        <Link href={'about-us'} className={`${vazir.className} Footer__rightSide__subtitle sm:text-right text-center text-white text-xl`}>
                             درباره ما
                         </Link>
-                        <Link href={'/products'} className='Footer__rightSide__subtitle sm:text-right text-center text-white text-xl'>
+                        <Link href={'/products'} className={`${vazir.className} Footer__rightSide__subtitle sm:text-right text-center text-white text-xl`}>
                             محصولات
                         </Link>
                     </div>
                     <div className='text-end flex flex-col gap-4'>
                         <h3 className={`Footer__rightSide__title sm:text-right text-center text-3xl ${myFont.className}`} style={{ color: '#EBDAB2' }}> قوانین </h3>
-                        <p className='Footer__rightSide__subtitle sm:text-right text-center text-white text-xl'>
+                        <p className={`${vazir.className} Footer__rightSide__subtitle sm:text-right text-center text-white text-xl`}>
                             قوانین و مقررات
                         </p>
-                        <p className='Footer__rightSide__subtitle sm:text-right text-center text-white text-xl'>
+                        <p className={`${vazir.className} Footer__rightSide__subtitle sm:text-right text-center text-white text-xl`}>
                             شرایط و موارد قانونی
                         </p>
                     </div>
@@ -46,7 +49,7 @@ const Footer = () => {
                 <div className='Footer_leftSide flex flex-col gap-16 text-right justify-center px-6'>
                     <h2 className={`text-white text-5xl md:text-6xl text-center ${myFont.className}`}> با ما در ارتباط باشید </h2>
                     <div className='Footer_leftSide__email flex flex-row-reverse items-center justify-between w-10/12 mx-auto md:ml-auto gap-6'>
-                        <input placeholder='ایمیل شما' className='outline-none bg-transparent text-white placeholder:text-white pb-1 text- placeholder:text-right w-full'
+                        <input placeholder='ایمیل شما' className={`${vazir.className} outline-none bg-transparent text-white placeholder:text-white pb-1 text- placeholder:text-right w-full`}
                             style={{ borderBottom: '1px solid white;', direction: 'ltr' }}
                         />
                         <div className='rounded-full bg-white'>
@@ -56,7 +59,7 @@ const Footer = () => {
                 </div>
             </div>
             <hr className='mt-16' />
-            <p className='text-white text-center mt-6 text-lg'> طراحی شده توسط زفره مدیا </p>
+            <p className={`text-white text-center mt-6 text-lg ${vazir.className}`}> طراحی شده توسط زفره مدیا </p>
         </div>
     );
 };
